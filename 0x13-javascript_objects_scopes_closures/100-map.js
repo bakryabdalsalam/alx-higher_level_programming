4e0ef3f0c588a82148a936bded4538f0d15f9f95
bakryabdalsalam <aymanbeko66@gmail.com>
2023-06-13 09:45:51 -0700

    fix error in file 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,7 +3,9 @@
 
 const originalList = require('./100-data').list;
 console.log(originalList);
-const mappedList = originalList.map (function (e, index) {
-    return (e * index);
+
+const mappedList = originalList.map(function(e, index) {
+  return e * index;
 });
+
 console.log(mappedList);
