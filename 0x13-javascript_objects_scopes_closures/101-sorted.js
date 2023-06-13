174de40b8d500485b8098ae6fad8739e25c04938
bakryabdalsalam <aymanbeko66@gmail.com>
2023-06-13 09:37:05 -0700

    fix error in file 101-sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,23 @@
 #!/usr/bin/node
 // function that returns the number of occurrences in a list
 
-const originalList = require('./100-data').list;
-console.log(originalList);
-const mappedList = originalList.map (function (e, index) {
-  return (e * index);
-});
-console.log(mappedList);
+const { dict } = require('./101-data.js');
+
+function computeUserIdsByOccurrence(dictionary) {
+  const userIdsByOccurrence = {};
+
+  for (const userId in dictionary) {
+    const occurrence = dictionary[userId];
+    if (userIdsByOccurrence[occurrence]) {
+      userIdsByOccurrence[occurrence].push(userId);
+    } else {
+      userIdsByOccurrence[occurrence] = [userId];
+    }
+  }
+
+  return userIdsByOccurrence;
+}
+
+const userIdsByOccurrence = computeUserIdsByOccurrence(dict);
+console.log(userIdsByOccurrence);
+
