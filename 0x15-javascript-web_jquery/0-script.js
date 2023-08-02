34239e47ae0213c361ae21ae2855f431a4c4897f
bakryabdalsalam <bakryabdalsalam6@gmail.com>
2023-08-02 09:58:20 -0700

    add the first two task

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// updates the text color of the <header> element to red (#FF0000)
+document.addEventListener("DOMContentLoaded", function () {
+  const headerElement = document.querySelector("header");
+  headerElement.style.color = "#FF0000";
+});
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,4 @@
+// updates the text color of the <header> element to red (#FF0000) using JQuery API
+$(document).ready(function () {
+  $("header").css("color", "#FF0000");
+});
